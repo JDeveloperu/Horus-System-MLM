1d2358f98ae2f38b48faa16d0321b45ae3092b0d
JDevevloperu <65090035+JDeveloperu@users.noreply.github.com>
2023-03-29 16:07:27 -0500

    Add files via upload

---
+++ datos_depositos.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+include("conexion.php");
+
+
+
+    $sql = "SELECT * FROM cards_info";
+    $resultado =  $mysqli->query($sql);
+    
+    if ($resultado){
+        while($row = $resultado->fetch_array()){
+            $id_card = $row['id_card'];
+            $usuario = $row['usuario'];
+            $monto = $row['monto'];
+            $recompensa_obtenida = $row['recompensa_obtenida'];
+            $contrato =$row['contrato'];
+        }
+    }
+
+
+?>
